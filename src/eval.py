384b39a299a5277dab5078480ccf1bf2a11e5644
Neil Ramaswamy <neil@ramaswamy.org>
2021-11-30 20:49:06 -0500

    Add t-SNE visualization

--- src/eval.py
+++ src/eval.py
@@ -1,8 +1,9 @@
-# write in here calculation of stats for evaluation (distances within a class, closest to other classes, etc)
-# as well as tsne
 import tensorflow as tf
 import matplotlib as plt
 import numpy as np
+
+from sklearn.manifold import TSNE
+from matplotlib import pyplot as plt
 
 from knn import knn_negative, knn_positive
 
@@ -19,9 +20,76 @@ def stats(model, identities_x):
             z = identities_z[identity_idx, print_idx, :]
             closest_neg_dist[identity_idx, print_idx] = tf.norm(closest_neg_z - z)
             closest_pos_dist[identity_idx, print_idx] = tf.norm(closest_pos_z - z)
-    print(f'Average closest negative distance: {np.mean(closest_neg_dist)}')
-    print(f'Average closest positive distance: {np.mean(closest_pos_dist)}')
+    print(f"Average closest negative distance: {np.mean(closest_neg_dist)}")
+    print(f"Average closest positive distance: {np.mean(closest_pos_dist)}")
 
 
+# Adapted from: https://datascientyst.com/get-list-of-n-different-colors-names-python-pandas/
+def generate_n_hex_colors(n: int):
+    colors = [""] * n
+    for i in range(n):
+        colors[i] = "#%06x" % np.random.randint(0, 0xFFFFFF)
+    return colors
 
-        
+
+def show_tsne_visualization(training_z: tf.Tensor, testing_z: tf.Tensor):
+    training_z = training_z[:10]
+    testing_z = testing_z[:10]
+
+    tsne = TSNE(n_components=2, random_state=0)
+
+    if training_z.shape[0] != testing_z.shape[0]:
+        raise ValueError(
+            "Training and testing sets must have the same number of identities"
+        )
+    num_identities = training_z.shape[0]
+
+    # Concatenate the training/testing latent spaces so we run the same t-SNE on both
+    # (Recall that t-SNE uses non-convex optimization, so we can't run it on both independently)
+    #
+    # Shape: (50, 5, 300)
+    all_z = tf.concat([training_z, testing_z], axis=1)
+    print(f"all_z shape: {all_z.shape}")
+
+    # Shape: (250, 2)
+    all_z_2d = tsne.fit_transform(tf.reshape(all_z, [-1, all_z.shape[-1]]))
+    print(f"all_z_2d shape: {all_z_2d.shape}")
+
+    # Separate the training/testing within the 2D space
+    # Should be of shape: (50, 5, 2)
+    all_2d = tf.reshape(all_z_2d, [all_z.shape[0], all_z.shape[1], 2])
+    print(f"all_2d shape: {all_2d.shape}")
+
+    # Shape: (50, 3, 2), (50, 2, 2)
+    training_2d, testing_2d = tf.split(
+        all_2d, [training_z.shape[1], testing_z.shape[1]], axis=1
+    )
+    print(
+        f"training_2d shape: {training_2d.shape}, testing_2d shape: {testing_2d.shape}"
+    )
+
+    colors = generate_n_hex_colors(num_identities)
+
+    # Plot the training set
+    for i, color in zip(range(num_identities), colors):
+        plt.scatter(
+            training_2d[i, :, 0],
+            training_2d[i, :, 1],
+            c=color,
+            label=f"Identity {i}",
+            alpha=0.5,
+        )
+
+    # Plot the testing set
+    for i, color in zip(range(num_identities), colors):
+        # Don't include a label for the testing set because it's already there from training
+        plt.scatter(
+            testing_2d[i, :, 0],
+            testing_2d[i, :, 1],
+            c=color,
+            alpha=0.5,
+            marker="s",
+        )
+
+    plt.legend()
+    plt.show()
--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 import numpy as np
 from time import time
 from MathworksLoader import MathworksLoader
-from eval import stats
+from eval import stats, show_tsne_visualization
 from model import FingNet
 from triplets import create_triplets_batch
 
-N_BATCHES = 1000
+N_BATCHES = 1
 N_IDENTITIES = 10
 N_ANCHOR_PER_IDENTITY = 3
 N_POS_PER_ANCHOR = 2
@@ -18,43 +18,58 @@ ALPHA = 0.3
 LAMBDA = 1
 D_LATENT = 300
 
-LEARNING_RATE = .1
+LEARNING_RATE = 0.1
+
 
 def sample_prints(identities_x, n_identities, n_prints_per_identity):
     # Sample anchors from positives
-    sample_identities_indices = np.random.permutation(
-        np.arange(identities_x.shape[0]))[:n_identities]
+    sample_identities_indices = np.random.permutation(np.arange(identities_x.shape[0]))[
+        :n_identities
+    ]
     # Make same size as prints indices
     sample_identities_indices = tf.expand_dims(sample_identities_indices, 1)
-    sample_identities_indices = tf.repeat(sample_identities_indices, n_prints_per_identity, axis=1)
+    sample_identities_indices = tf.repeat(
+        sample_identities_indices, n_prints_per_identity, axis=1
+    )
     samples_prints_indices = np.empty([n_identities, identities_x.shape[1]])
     for i in range(n_identities):
         samples_prints_indices[i] = np.random.permutation(
-            np.arange(identities_x.shape[1]))[:n_prints_per_identity]
-    sample_indices = tf.stack([sample_identities_indices, samples_prints_indices], axis=2)
+            np.arange(identities_x.shape[1])
+        )[:n_prints_per_identity]
+    sample_indices = tf.stack(
+        [sample_identities_indices, samples_prints_indices], axis=2
+    )
     sample_identities_x = tf.gather_nd(identities_x, sample_indices)
-    
+
     return sample_identities_x
 
+
 loader = MathworksLoader(IMAGE_HEIGHT, IMAGE_WIDTH)
-loader.load_fingerprints('./data', 0.6)
+loader.load_fingerprints("./data", 0.6)
 identities_x_train = loader.train_fingerprints
 
 model = FingNet(ALPHA, LAMBDA, D_LATENT)
 optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
 
 for i in range(N_BATCHES):
-    sample_identities_x = sample_prints(identities_x_train, N_IDENTITIES, N_ANCHOR_PER_IDENTITY)
+    sample_identities_x = sample_prints(
+        identities_x_train, N_IDENTITIES, N_ANCHOR_PER_IDENTITY
+    )
     with tf.GradientTape() as tape:
         sample_identities_z = model.call_on_identities(
-            sample_identities_x, training=True)
-        z_a, z_p, z_n = create_triplets_batch(
-            sample_identities_z, N_POS_PER_ANCHOR)
+            sample_identities_x, training=True
+        )
+        z_a, z_p, z_n = create_triplets_batch(sample_identities_z, N_POS_PER_ANCHOR)
         loss = model.loss_function(z_a, z_p, z_n)
         print(loss.numpy())
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-stats(model, loader.train_fingerprints)
-stats(model, loader.test_fingerprints)
+# stats(model, loader.train_fingerprints)
+# stats(model, loader.test_fingerprints)
 
-tf.keras.models.save_model(model, './models/fing')
+
+show_tsne_visualization(
+    model.call_on_identities(loader.train_fingerprints, training=True),
+    model.call_on_identities(loader.test_fingerprints, training=True),
+)
+# tf.keras.models.save_model(model, "./models/fing")
